cd81bda375323392208268ba945a06c48e73118b
Shaun <swakashige@wontonwebworks.com>
2024-11-22 19:59:03 -0800

    weird issue with closeHandler not working. Placed console.log for debugging.

--- app/routes/expenses.add.jsx
+++ app/routes/expenses.add.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '../components/expenses/ExpenseForm';
 import Modal from '../components/util/Modal';
 
 export default function AddExpensePage() {
+  const navigate = useNavigate();
+
+  function closeHandler() { 
+    // navigate back
+    console.log('closeHandler');
+    navigate('..');
+  }
+
   return (
-    <Modal>
+    <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
