443462bd856ceddcb108117021787a13365dfd10
Shaun <swakashige@wontonwebworks.com>
2024-11-25 23:40:13 -0800

    add server side validation.

--- app/routes/__app.expenses.add.jsx
+++ app/routes/__app.expenses.add.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '../components/expenses/ExpenseForm';
 import Modal from '../components/util/Modal';
 import { addExpense } from '../data/expenses.server';
+import { validateExpenseInput } from '../data/validation.server';
 
 export default function AddExpensePage() {
   const navigate = useNavigate();
@@ -24,6 +25,12 @@ export async function action({request}) {
   const formData = await request.formData();
   const expenseData = Object.fromEntries(formData);
   console.log(expenseData, formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
 
   await addExpense(expenseData);
   return redirect('/expenses');
